2b72cc74207150169de9d89280aa0d7d42bdf42c
Reinier Hernández <sasuke.reinier@gmail.com>
2021-06-20 19:41:28 -0400

    agregando tailwind

---
+++ src/components/layouts/dashboard.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const DashboardLayout = ({ children }) => <div>{children}</div>;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: "media",
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
+};
